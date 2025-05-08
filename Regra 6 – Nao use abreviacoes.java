5d3b6f69058d6bd801e60b6f56d7154034265b33
jppaschoalini <163948231+jppaschoalini@users.noreply.github.com>
2025-05-08 18:41:21 -0300

    parte sobre abreviacoes

---
+++ Regra 6 – Nao use abreviacoes.java
@@ -0,0 +1,23 @@
+//Exemplo antes de usar Objetos Calistrnicos (com abreviações):
+
+
+public class Cstmr {
+    private String nm;
+
+    public String getNm() {
+        return nm;
+    }
+}
+
+
+
+//Exemplo depois de usar Objetos Calistrnicos (sem abreviações):
+
+
+public class Cliente {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+}
